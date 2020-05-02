3b2c388ebceafa9327597d87a773e56dcf090d29
Ferdinand <fercalo96@yahoo.de>
2020-05-02 18:17:42 +0200

    New Radio1 Model & Updated Radio1 Block.

--- src/main/java/net/fexcraft/mod/frsm/blocks/machines/Radio1.java
+++ src/main/java/net/fexcraft/mod/frsm/blocks/machines/Radio1.java
@@ -3,17 +3,16 @@ package net.fexcraft.mod.frsm.blocks.machines;
 import net.fexcraft.lib.mc.api.registry.fBlock;
 import net.fexcraft.mod.frsm.FRSM;
 import net.fexcraft.mod.frsm.util.FRSMTabs;
-import net.fexcraft.mod.frsm.util.block.BasicContainer4R;
+import net.fexcraft.mod.frsm.util.block.Basic4R;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-@fBlock(modid = FRSM.MODID, name = "radio1", tileentity = Radio1.Entity.class)
-public class Radio1 extends BasicContainer4R {
+@fBlock(modid = FRSM.MODID, name = "radio1")
+public class Radio1 extends Basic4R {
 	
 	public Radio1() {
     	super(Material.GLASS);
@@ -24,14 +23,12 @@ public class Radio1 extends BasicContainer4R {
 	}
     
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos){
-		return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 1.0F, 0.4F, 1.0F);
+		return new AxisAlignedBB(0.25F, 0.0F, 0.25F, 0.75F, 0.375F, 0.75F);
 	}
-	
+
 	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
-		return new Entity();
-	}
-	
-	public static class Entity extends TileEntity {}
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World world, BlockPos pos){
+        return getBoundingBox(state, world, pos).offset(pos);
+    }
 	
 }
--- src/main/java/net/fexcraft/mod/frsm/models/Radio1Model.java
+++ src/main/java/net/fexcraft/mod/frsm/models/Radio1Model.java
@@ -1,64 +1,86 @@
-// Date: 21.02.2015 15:06:19
-
+//FMT-Marker FVTM-1.4
 package net.fexcraft.mod.frsm.models;
 
-import net.fexcraft.lib.mc.api.registry.fTESR;
-import net.fexcraft.mod.frsm.blocks.machines.Radio1;
-import net.fexcraft.mod.frsm.util.block.TileRenderer4R;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.entity.Entity;
+import net.fexcraft.lib.mc.api.registry.fModel;
+import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.fexcraft.mod.frsm.util.FVTMFormatModel;
 
-public class Radio1Model extends ModelBase{
+/** This file was exported via the FVTM Exporter V1.4 of<br>
+ *  FMT (Fex's Modelling Toolbox) v.2.4.1 &copy; 2020 - Fexcraft.net<br>
+ *  All rights reserved. For this Model's License contact the Author/Creator.
+ *  
+ *  @author Ferdinand (FEX___96)
+ */
+@fModel(registryname = "frsm:models/block/radio1")
+public class Radio1Model extends FVTMFormatModel {
 
-    ModelRenderer Shape1;
-    ModelRenderer Shape2;
-
-    public Radio1Model(){
-    	textureWidth = 32; textureHeight = 32;
-    	Shape1 = new ModelRenderer(this, 0, 0);
-    	Shape1.addBox(0F, 0F, 0F, 8, 4, 2);
-    	Shape1.setRotationPoint(-4F, 20F, -1F);
-    	Shape1.setTextureSize(32, 32);
-    	Shape1.mirror = true;
-    	setRotation(Shape1, 0F, 0F, 0F);
-    	Shape2 = new ModelRenderer(this, 0, 7);
-    	Shape2.addBox(0F, 0F, 0F, 8, 1, 0);
-    	Shape2.setRotationPoint(-4F, 20F, 1F);
-    	Shape2.setTextureSize(32, 32);
-    	Shape2.mirror = true;
-    	setRotation(Shape2, 0F, 0F, -0.8726646F);
-    }
-
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5){
-    	super.render(entity, f, f1, f2, f3, f4, f5);
-    	setRotationAngles(f, f1, f2, f3, f4, f5);
-    	Shape1.render(f5);
-    	Shape2.render(f5);
-    }
-    
-    private void setRotation(ModelRenderer model, float x, float y, float z){
-    	model.rotateAngleX = x;
-    	model.rotateAngleY = y;
-    	model.rotateAngleZ = z;
-    }
-    
-    public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5){
-    	super.setRotationAngles(f, f1, f2, f3, f4, f5, null);
-    }
-
-	public static Radio1Model INSTANCE = new Radio1Model();
-    
-    @fTESR
-    public static class Renderer extends TileRenderer4R<Radio1.Entity> {
-    	
-    	public Renderer(){ super("frsm:textures/blocks/Radio1.png", null); }
-
-    	@Override
-    	public void renderModel(Radio1.Entity tileentity, float partialticks, int destroystage){
-    		INSTANCE.render(null, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-    	}
-    	
-    }
+	public Radio1Model(){
+		super(); textureX = 32; textureY = 32;
+		//
+		TurboList group0 = new TurboList("group0");
+		group0.add(new ModelRendererTurbo(group0, 0, 7, textureX, textureY)
+			.addBox(0, 0, 0, 2, 4, 8, 0, 1f, new boolean[]{ false, false, false, false, true, true })
+			.setRotationPoint(-1, -4, -4).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 0, 20, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 8, 0, 0, 0, 0, -0.75f, 0, 0, -0.75f, 0, 0, 0, 0, 0, 0, -0.875f, 0, -0.75f, -0.875f, 0, -0.75f, -0.875f, 0, 0, -0.875f, 0)
+			.setRotationPoint(-0.875f, -4, -4).setRotationAngle(8, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 9, 0, textureX, textureY).addHollowCylinder(0, 0, 0, 2, 1.75f, 1, 12, 0, 0.875f, 0.75f, 3,
+			null, new boolean[]{ true, false, false, false })
+			.setRotationPoint(0.125f, -2, -2.0625f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 0, 10, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 1, 0, -0.25f, -0.5f, -0.5f, -0.25f, -0.5f, -0.5f, -0.25f, -0.5f, 0, -0.25f, -0.5f, 0, -0.25f, 0, 0, -0.25f, 0, 0, -0.25f, 0, 0, -0.25f, 0, 0,
+				new boolean[]{ false, false, false, true, false, false })
+			.setRotationPoint(-0.5f, -5, -3.5f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 17, 18, textureX, textureY)
+			.addBox(0, 0, 0, 1, 3, 2, 0, 1f, new boolean[]{ false, true, false, false, false, false })
+			.setRotationPoint(0.0625f, -3.5f, 1.75f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 24, 0, textureX, textureY).addHollowCylinder(0, 0, 0, 1, 0.001f, 1, 16, 8, 0.5f, 0.5f, 2,
+			new net.fexcraft.lib.common.math.Vec3f(0.5, 0.0, 0.0), new boolean[]{ false, false, false, true })
+			.setRotationPoint(-0.75f, -2.75f, 4).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 0, 0, textureX, textureY).addHollowCylinder(0, 0, 0, 2, 0.25f, 1, 12, 0, 0.6875f, 0.6875f, 3,
+			null, new boolean[]{ true, false, true, false })
+			.setRotationPoint(0.0625f, -2, -2.0625f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 13, 7, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 1, 0, -0.25f, -0.5f, 0, -0.25f, -0.5f, 0, -0.25f, -0.5f, -0.5f, -0.25f, -0.5f, -0.5f, -0.25f, 0, 0, -0.25f, 0, 0, -0.25f, 0, 0, -0.25f, 0, 0,
+				new boolean[]{ false, false, false, true, false, false })
+			.setRotationPoint(-0.5f, -5, 2.5f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 17, 11, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 5, 0, -0.25f, -0.5f, 0, -0.25f, -0.5f, 0, -0.25f, -0.5f, 0, -0.25f, -0.5f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0,
+				new boolean[]{ false, false, false, false, true, true })
+			.setRotationPoint(-0.5f, -5, -2.5f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 25, 5, textureX, textureY)
+			.addShapeBox(0, 0, 0, 2, 4, 1, 0, 0, -0.5f, 0, 0, -0.5f, 0, 0, 0, -0.75f, 0, 0, -0.75f, 0, -0.5f, 0, 0, -0.5f, 0, 0, 0, -0.75f, 0, 0, -0.75f,
+				new boolean[]{ false, false, false, false, false, true })
+			.setRotationPoint(-1, -4, -4.25f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 18, 5, textureX, textureY)
+			.addShapeBox(0, 0, 0, 2, 4, 1, 0, 0, 0, -0.75f, 0, 0, -0.75f, 0, -0.5f, 0, 0, -0.5f, 0, 0, 0, -0.75f, 0, 0, -0.75f, 0, -0.5f, 0, 0, -0.5f, 0)
+			.setRotationPoint(-1, -4, 3.25f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 4, 7, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 1, 0, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f,
+				new boolean[]{ false, true, false, false, false, false })
+			.setRotationPoint(0.0625f, -3.75f, 0.75f).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 0, 7, textureX, textureY)
+			.addShapeBox(0, 0, 0, 1, 1, 1, 0, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f, 0, -0.25f, -0.25f,
+				new boolean[]{ false, true, false, false, false, false })
+			.setRotationPoint(0.0625f, -3.75f, 0).setRotationAngle(0, 0, 0)
+		);
+		group0.add(new ModelRendererTurbo(group0, 18, 0, textureX, textureY).addHollowCylinder(0, 0, 0, 1, 0.001f, 1, 16, 8, 0.5f, 0.5f, 2,
+			new net.fexcraft.lib.common.math.Vec3f(0.5, 0.0, 0.0), new boolean[]{ false, false, false, true })
+			.setRotationPoint(-0.75f, -1.25f, 4).setRotationAngle(0, 0, 0)
+		);
+		this.groups.add(group0);
+	}
 
 }
